bb40df1d0eb7f436aa5283f7ab8fc9745e2b4c19
Zach Boras <zrboras@gmail.com>
2021-01-05 01:15:51 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,43 @@
+ let firstNode = ['Alexandra', 1]
+ let secondNode = ['Kirstin', 2]
+ let thirdNode = ['Juliet', 3]
+ let fourthNode =['Timmy', 4]
+ let fifthNode =['Jacob', null]
+ 
+ let collection = {0: firstNode, 1: secondNode, 2: thirdNode, 3: fourthNode,4: fifthNode}
+
+
+function getName() {
+  
+}
+
+
+function headNode() {
+  
+}
+
+
+function next(node){
+
+}
+
+
+function nodeAt(index) {
+
+}
+
+function addressAt() {
+  
+}
+
+function indexAt(node) {
+  
+}
+
+function insertNodeAt() {
+  
+}
+
+function deleteNodeAt() {
+  
+}
